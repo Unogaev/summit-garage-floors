946d7fe3b61158fb0f9ae962909c7db75cd0f003
Unogaev <84148979+Unogaev@users.noreply.github.com>
2026-05-09 18:00:26 -0400

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,52 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.summitgaragefloors.com",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog/garage-floor-coating-bozeman-mt",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog/garage-floor-coating-whitefish-mt",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog/garage-floor-coating-big-sky-mt",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog/polyaspartic-vs-epoxy-montana",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog/how-to-care-for-garage-floor-coating",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.summitgaragefloors.com/blog/garage-floor-coating-faq",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
